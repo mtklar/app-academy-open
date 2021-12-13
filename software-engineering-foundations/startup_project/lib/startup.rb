9d3d7e750b0692466c2c5b749e20f70e4d062abb
Daniel Fenner <dcfenner@gmail.com>
2021-12-13 09:00:48 -0500

    Stuck on this

---
+++ software-engineering-foundations/startup_project/lib/startup.rb
@@ -0,0 +1,75 @@
+require "employee"
+
+class Startup
+
+    attr_reader :name, :funding, :salaries, :employees
+
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+    def valid_title?(title)
+        @salaries.key?(title)
+    end
+
+    def >(startup)
+        if self.funding > startup.funding
+            return true
+        else
+            return false
+        end
+    end
+
+    def hire(employee_name, title)
+        if self.valid_title?(title)
+            employees << Employee.new(employee_name, title)
+        else
+            raise "Not a valid title"
+        end
+    end
+
+    def size
+        @employees.length
+    end
+
+    def pay_employee(employee)
+        pay_amount = @salaries[employee.title]
+        if @funding > pay_amount
+            employee.pay(pay_amount)
+            @funding -= pay_amount
+        else
+            raise "not enough money"
+        end
+    end
+
+    def payday
+        @employees.each do |ele|
+            pay_employee(ele)
+        end
+    end
+
+    def average_salary
+        salaries_total = []
+
+        @employees.each do |ele|
+            salaries_total << @salaries[ele.title]
+        end
+
+        salaries_total.sum / salaries_total.length
+    end
+
+    def close
+        @employees = []
+        @funding = 0
+    end
+
+    # Stuck on this
+    def acquire(other_startup)
+        @funding += other_startup.funding
+        @salaries.merge(other_startup.salaries)
+    end
+
+end
