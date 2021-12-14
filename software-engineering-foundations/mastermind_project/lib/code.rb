95dd3e9bd308f7db45c9fca00dc91d729c6c5eb5
Daniel Fenner <dcfenner@gmail.com>
2021-12-14 18:59:09 -0500

    They had an error in the spec, what a nightmare

--- software-engineering-foundations/mastermind_project/lib/code.rb
+++ software-engineering-foundations/mastermind_project/lib/code.rb
@@ -52,37 +52,16 @@ class Code
   def num_exact_matches(code_instance)
     count = 0
     code_instance.pegs.each.with_index do |ele, i|
-      if ele == @pegs[i]
-        count += 1
-      end
+      count += 1 if ele == self[i]
     end
-    return count
+    count
   end
-
 
   def num_near_matches(code_instance)
     count = 0
-    pegs_dupe = @pegs.dup
-    code_pegs_dupe = code_instance.pegs.dup
-    
-    puts "pre pegs:"
-    p pegs_dupe
-    puts "pre code pegs:"
-    p code_pegs_dupe
 
-    code_pegs_dupe.each.with_index do |ele, i|
-      if ele == pegs_dupe[i]
-        pegs_dupe[i] = "n"
-        code_pegs_dupe[i] = "x"
-      end
-    end
-    puts "post pegs: "
-    p pegs_dupe
-    puts "post code pegs:"
-    p code_pegs_dupe
-
-    code_pegs_dupe.each.with_index do |ele, i|
-      if pegs_dupe.include?(ele)
+    code_instance.pegs.each.with_index do |ele, i|
+      if ele != self[i] && self.pegs.include?(ele)
         count += 1
       end
     end
@@ -90,35 +69,8 @@ class Code
     count
   end
 
-  # # RGRB
-  # # RGBB
-
-  # # R true false
-  # # G true false
-  # # 
-  # def num_near_matches(code_instance)
-  #   count = 0
-  #   pegs_copy = @pegs
-  #   code_instance.pegs.each.with_index do |ele, i|
-  #     p pegs_copy
-  #     p code_instance.pegs
-  #     if pegs_copy.include?(ele) && ele != pegs_copy[i]
-  #       p ele
-  #       p pegs_copy.include?(ele)
-  #       p ele != pegs_copy[i]
-  #       count += 1
-  #     end
-  #     # if pegs_copy.index(ele) != nil
-  #     #   pegs_copy[pegs_copy.index(ele)] = ""
-  #     # end
-  #     p pegs_copy
-  #   end
-  #   p num_exact_matches(code_instance)
-  #   if count - num_exact_matches(code_instance) <= 0
-  #     return 0
-  #   else
-  #     return count - num_exact_matches(code_instance)
-  #   end
-  # end
+  def ==(code_instance)
+   self.pegs == code_instance.pegs
+  end
 
 end
