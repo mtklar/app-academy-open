ceecde39b6d83177e4252d2ab042e133d5e10f6d
Daniel Fenner <dcfenner@gmail.com>
2021-12-21 09:00:37 -0500

    Continue from first_index next time

--- software-engineering-foundations/perilous-procs/perilous-procs.rb
+++ software-engineering-foundations/perilous-procs/perilous-procs.rb
@@ -23,3 +23,59 @@ def exactly?(arr, n, &prc)
         return false
     end
 end
+
+def filter_out(arr, &prc)
+    new_array = []
+
+    arr.each do |ele|
+        if !prc.call(ele)
+            new_array << ele
+        end
+    end
+
+    new_array
+end
+
+def at_least?(arr, n, &prc)
+    count = 0
+
+    arr.each do |ele|
+        if prc.call(ele)
+            count += 1
+        end
+    end
+
+    if count >= n
+        return true
+    else 
+        return false
+    end
+end
+
+def every?(arr, &prc)
+    arr.each do |ele|
+        return false if !prc.call(ele)
+    end
+
+    return true
+end
+
+def at_most?(arr, n, &prc)
+    count = 0
+
+    arr.each do |ele|
+        if !prc.call(ele)
+            count += 1
+        end
+    end
+
+    if count > n
+        return true
+    else 
+        return false
+    end
+end
+
+def first_index
+
+end
