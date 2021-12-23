6a87931a1720f8a377d576f81694bcbc77e728e1
Daniel Fenner <dcfenner@gmail.com>
2021-12-23 08:15:13 -0500

    WORKING ON TIC TAC TOE EXCITING

---
+++ software-engineering-foundations/tic_tac_toe_v1/board.rb
@@ -0,0 +1,37 @@
+class Board
+
+    def initialize
+        @grid = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+    end
+
+    def valid?(position)
+        pos1 = position[0]
+        pos2 = position[1]
+        size = @grid.length - 1
+
+        if pos1 >= 0 && pos1 <= size && pos2 >= 0 && pos2 <= size
+            return true
+        else 
+            return false
+        end
+    end
+
+    def empty?(position)
+        if @grid[position[0]][position[1]] == "_"
+            return true
+        else
+            return false
+        end
+    end
+
+    def place_mark(position, mark)
+        if !valid?(position)
+            raise "Invalid Position"
+        elsif !empty?(position)
+            raise "That position has been taken already"
+        else
+            @grid[position[0]][position[1]] = mark
+        end
+    end
+
+end
