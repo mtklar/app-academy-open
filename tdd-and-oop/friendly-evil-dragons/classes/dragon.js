4d8ef4eadb333d8ba216740715b33b5e91f9d4ef
Daniel Fenner <dcfenner@gmail.com>
2022-08-15 11:16:23 -0400

    Dragons

---
+++ tdd-and-oop/friendly-evil-dragons/classes/dragon.js
@@ -0,0 +1,29 @@
+// Your code here
+
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    let newArr = [];
+    for (let i = 0; i < dragons.length; i++) {
+      newArr.push(dragons[i].name);
+    }
+    return newArr;
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Dragon;
+} catch {
+  module.exports = null;
+}
