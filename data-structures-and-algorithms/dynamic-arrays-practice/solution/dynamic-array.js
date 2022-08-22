7f721ab8dd1245c178b615b0fbed08f8f06b2f37
Daniel Fenner <dcfenner@gmail.com>
2022-08-22 10:59:38 -0400

    dynamic arrays solved

---
+++ data-structures-and-algorithms/dynamic-arrays-practice/solution/dynamic-array.js
@@ -0,0 +1,24 @@
+class DynamicArray {
+  constructor(defaultSize = 4) {
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
+  }
+
+  read(index) {
+    return this.data[index];
+  }
+
+  unshift(val) {
+    if (this.length === this.capacity) {
+      this.resize(this.capacity * 2);
+    }
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
+  }
+}
+
+module.exports = DynamicArray;
