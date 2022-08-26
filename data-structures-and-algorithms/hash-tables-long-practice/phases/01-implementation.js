884e609cbf69f61b7ccd978fb34def47b7041014
Daniel Fenner <dcfenner@gmail.com>
2022-08-26 12:59:43 -0400

    hash tables

--- data-structures-and-algorithms/hash-tables-long-practice/phases/01-implementation.js
+++ data-structures-and-algorithms/hash-tables-long-practice/phases/01-implementation.js
@@ -6,11 +6,15 @@ class KeyValuePair {
   }
 }
 
-class HashTable { // get O(1), set O(1), deleteKey O(1)
+class HashTable {
+  // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
     // Initialize your buckets here
     // Your code here
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets);
+    this.count = 0;
   }
 
   hash(key) {
@@ -28,26 +32,86 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     return this.hash(key) % this.capacity;
   }
 
+  insert(key, value, nocount = false) {
+    let newPair = new KeyValuePair(key, value);
+    let hash = this.hashMod(key);
+    if (!nocount) {
+      this.count++;
+    }
 
-  insert(key, value) {
-    // Your code here
+    if (this.capacity / this.count < 0.7) {
+      this.resize();
+    }
+
+    if (this.data[hash] === undefined) {
+      this.data[hash] = newPair;
+      return;
+    } else {
+      let curr = this.data[hash];
+      while (curr) {
+        if (curr.key === key) {
+          curr.value = value;
+          return;
+        } else {
+          curr = curr.next;
+        }
+      }
+    }
+
+    newPair.next = this.data[hash];
+    this.data[hash] = newPair;
   }
-
 
   read(key) {
-    // Your code here
+    let hash = this.hashMod(key);
+    let curr = this.data[hash];
+    while (curr) {
+      if (curr.key === key) {
+        return curr.value;
+      } else {
+        curr = curr.next;
+      }
+    }
+    return undefined;
   }
-
 
   resize() {
-    // Your code here
+    let dataCopy = [...this.data];
+    this.capacity *= 2;
+    this.data = new Array(this.capacity);
+    for (let i = 0; i < dataCopy.length; i++) {
+      if (dataCopy[i] === undefined) {
+        continue;
+      }
+      let el = dataCopy[i];
+      while (el) {
+        this.insert(el.key, el.value, true);
+        el = el.next;
+      }
+    }
   }
-
 
   delete(key) {
     // Your code here
   }
 }
 
+let hashTable = new HashTable(2);
+
+console.log("Reading and writing 50k items to hashtable:");
+
+let start = Date.now();
+
+for (let i = 0; i < 50000; i++) {
+  hashTable.insert(`key${i}`, `value${i}`);
+}
+for (let i = 0; i < 50000; i++) {
+  hashTable.read(`key${i}`);
+}
+
+let end = Date.now();
+console.log(end - start, "ms");
+// Reading and writing 50k items to hashtable:
+// 850ms
 
 module.exports = HashTable;
--- data-structures-and-algorithms/hash-tables-long-practice/phases/02-practice-problems.js
+++ data-structures-and-algorithms/hash-tables-long-practice/phases/02-practice-problems.js
@@ -1,26 +1,68 @@
 function anagrams(str1, str2) {
-  // Your code here
-}
+  let obj = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (obj[str1[i]]) {
+      obj[str1[i]]++;
+    } else {
+      obj[str1[i]] = 1;
+    }
+  }
 
+  for (let i = 0; i < str2.length; i++) {
+    if (obj[str2[i]]) {
+      obj[str2[i]]--;
+      if (obj[str2[i]] < 0) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 function commonElements(arr1, arr2) {
-  // Your code here
+  let obj = {};
+  let arr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (obj[arr1[i]]) {
+      continue;
+    } else {
+      obj[arr1[i]] = 1;
+    }
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    if (obj[arr2[i]]) {
+      arr.push(arr2[i]);
+    }
+  }
+  return arr;
 }
-
 
 function duplicate(arr) {
-  // Your code here
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]]) {
+      return arr[i];
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
 }
-
 
 function twoSum(nums, target) {
-  // Your code here
+  let obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (obj[nums[i]]) {
+      return true;
+    } else {
+      obj[target - nums[i]] = 1;
+    }
+  }
+  return false;
 }
 
-
-function wordPattern(pattern, strings) {
-  // Your code here
-}
-
+function wordPattern(pattern, strings) {}
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
